118a33a1c527af32965d2bbebd79de301e8e6f25
kshitij1127 <gudlakshitij.1127@gmail.com>
2021-07-30 17:52:01 +0530

    share files!

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing'
+import uploadToAnonymousFilesAsync from 'anonymous-files';
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let imagePickerAsync = async() => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert('You denied the request for permissions.');
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    
+    if (pickerResult.canceled) {
+      return;
+    }
+    
+    if (Platform.OS === 'web') {
+      let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
+      selectedImage({localUri: pickerResult.uri, remoteUri})
+    } else {
+      setSelectedImage({localUri: pickerResult.uri, remoteUri: null})
+    }
+  };
+
+  let openShareDialogAsync = async() => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert(`The image is available for sharing at: ${selectedImage.remoteUri}`);
+      return;
+    }
+
+    Sharing.shareAsync(selectedImage.localUri); 
+  }
+
+  if (selectedImage !== null) {
+    return(
+      <View style={styles.container}>
+        <Image source={{uri: selectedImage.localUri}} style={styles.thumbnail} />
+        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}>Share this photo!</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
+
+      <Text style={styles.instructions} >
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
+
+      <TouchableOpacity style={styles.button} onPress={imagePickerAsync}>
+        <Text style={styles.buttonText}>Share Photo!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,5 +70,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  }, 
+
+  button: {
+    backgroundColor: 'blue',
+    padding: 20,
+    borderRadius: 20,
+  }, 
+
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  },
+
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
   },
 });
